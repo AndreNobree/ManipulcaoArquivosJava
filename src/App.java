73b02674007f843506c08896bf624d50daa7bab3
André Nobre <afelipe1405@gmail.com>
2023-05-11 08:53:48 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class App {
 
-    int respNumber;
     public static void main(String[] args) throws Exception {
         Scanner entrada = new Scanner(System.in);
         
@@ -12,30 +11,38 @@ public class App {
         System.out.println("DIGITE 'java' SE FOR JAVA: ");
         System.out.print("DIGITE 'time' SE FOR TIME: \n>> ");
         chamaRecebe.resp = entrada.next().toLowerCase();
-        
-        switch (chamaRecebe.resp){
-            case "doc":
-                chamaRecebe.menuDoc();
-                break;
+        //chamaRecebe.resp está na classe Recebe, pq eu n declarei o resp aqui? sei lá kkkkk
+        try {
+            switch (chamaRecebe.resp){
+                case "doc":
+                    chamaRecebe.menuDoc();
+                    break;
+    
+                case "java":
+                    chamaRecebe.menuJava();
+                    break;
+    
+                case "time":
+                    chamaRecebe.menuTime();
+                    break;
+    
+                default:
+                    System.out.println("COMANDO INVÁLIDO");
+                    return;//se caso errar, vai repetir o case
+            }
+            chamaRecebe.carrega();
+            chamaRecebe.pegaArquivo();
+            
+            LerArquivoTxt chamaLerArquivoTxt = new LerArquivoTxt();
+            chamaLerArquivoTxt.abreArquivo();
 
-            case "java":
-                chamaRecebe.menuJava();
-                break;
+        }catch (Exception e) {
+            System.out.println("Ocorreu um erro ao executar o programa: " + e.getMessage());
+            e.printStackTrace();
 
-            case "time":
-                chamaRecebe.menuTime();
-                break;
-
-            default:
-                System.out.println("COMANDO INVÁLIDO");
-                break;
+        } finally {
+            entrada.close();
         }
-        chamaRecebe.carrega();
-        chamaRecebe.pegaArquivo();
-        
-        LerArquivoTxt chamaLerArquivoTxt = new LerArquivoTxt();
-        chamaLerArquivoTxt.abreArquivo();
 
-        entrada.close();
     }
 }
